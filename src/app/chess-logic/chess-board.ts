5d80e0ca5e783e9c6033dff7d7a5c7a8ccf68478
Mark Mysler <myslermark@gmail.com>
2024-05-12 23:42:37 -0300

    primer commit

---
+++ src/app/chess-logic/chess-board.ts
@@ -0,0 +1,230 @@
+import { Color, Coords, FENChar, SafeSquares } from './models';
+import { Bishop } from './pieces/bishop';
+import { King } from './pieces/king';
+import { Knight } from './pieces/knight';
+import { Pawn } from './pieces/pawn';
+import { Piece } from './pieces/piece';
+import { Queen } from './pieces/queen';
+import { Rook } from './pieces/rook';
+
+export class ChessBoard {
+  private chessBoard: (Piece | null)[][];
+  private readonly chessBoardSize = 8;
+  private _playerColor = Color.White;
+  private _safeSquares: SafeSquares;
+
+  constructor() {
+    this.chessBoard = [
+      [
+        new Rook(Color.White),
+        new Knight(Color.White),
+        new Bishop(Color.White),
+        new Queen(Color.White),
+        new King(Color.White),
+        new Bishop(Color.White),
+        new Knight(Color.White),
+        new Rook(Color.White),
+      ],
+      [
+        new Pawn(Color.White),
+        new Pawn(Color.White),
+        new Pawn(Color.White),
+        new Pawn(Color.White),
+        new Pawn(Color.White),
+        new Pawn(Color.White),
+        new Pawn(Color.White),
+        new Pawn(Color.White),
+      ],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [
+        new Pawn(Color.Black),
+        new Pawn(Color.Black),
+        new Pawn(Color.Black),
+        new Pawn(Color.Black),
+        new Pawn(Color.Black),
+        new Pawn(Color.Black),
+        new Pawn(Color.Black),
+        new Pawn(Color.Black),
+      ],
+      [
+        new Rook(Color.Black),
+        new Knight(Color.Black),
+        new Bishop(Color.Black),
+        new Queen(Color.Black),
+        new King(Color.Black),
+        new Bishop(Color.Black),
+        new Knight(Color.Black),
+        new Rook(Color.Black),
+      ],
+    ];
+    this._safeSquares = this.findSafeSqares();
+  }
+
+  public get playerColor(): Color {
+    return this._playerColor;
+  }
+
+  public get chessBoardView(): (FENChar | null)[][] {
+    return this.chessBoard.map((row) => {
+      return row.map((piece) =>
+        piece instanceof Piece ? piece.FENChar : null
+      );
+    });
+  }
+
+  public get safeSquares(): SafeSquares {
+    return this._safeSquares;
+  }
+
+  public static isSquareDark(x: number, y: number): boolean {
+    return (x % 2 === 0 && y % 2 === 0) || (x % 2 === 1 && y % 2 === 1);
+  }
+
+  private areCoordsValid(x: number, y: number): boolean {
+    return (
+      x >= 0 && y >= 0 && x < this.chessBoardSize && y < this.chessBoardSize
+    );
+  }
+
+  public isInCheck(playerColor: Color): boolean {
+    for (let x = 0; x < this.chessBoardSize; x++) {
+      for (let y = 0; y < this.chessBoardSize; y++) {
+        const piece: Piece | null = this.chessBoard[x][y];
+        if (!piece || piece.color === playerColor) continue;
+        for (const { x: dx, y: dy } of piece.directions) {
+          let newX: number = x + dx;
+          let newY: number = y + dy;
+
+          if (!this.areCoordsValid(newX, newY)) continue;
+          if (
+            piece instanceof Pawn ||
+            piece instanceof Knight ||
+            piece instanceof King
+          ) {
+            //Los peones solo atacan diagonalmente
+            if (piece instanceof Pawn && dy === 0) continue;
+
+            const attackedPiece: Piece | null = this.chessBoard[newX][newY];
+            if (
+              attackedPiece instanceof King &&
+              attackedPiece.color === playerColor
+            )
+              return true;
+          } else {
+            while (this.areCoordsValid(newX, newY)) {
+              const attackedPiece: Piece | null = this.chessBoard[newX][newY];
+              if (
+                attackedPiece instanceof King &&
+                attackedPiece.color === playerColor
+              )
+                return true;
+
+              if (attackedPiece !== null) break;
+
+              newX += dx;
+              newY += dy;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
+  private isPositionSafeAfterMove(
+    piece: Piece,
+    prevX: number,
+    prevY: number,
+    newX: number,
+    newY: number
+  ): boolean {
+    const newPiece: Piece | null = this.chessBoard[newX][newY];
+
+    //no podemos mover una pieza donde ya hay otra del mismo color
+    if (newPiece && newPiece.color === piece.color) return false;
+
+    //simular posicion
+    this.chessBoard[prevX][prevY] = null;
+    this.chessBoard[newX][newY] = piece;
+
+    const isPositionSafe: boolean = !this.isInCheck(piece.color);
+
+    // restaurar posicion original
+    this.chessBoard[prevX][prevY] = piece;
+    this.chessBoard[newX][newY] = newPiece;
+
+    return isPositionSafe;
+  }
+
+  private findSafeSqares(): SafeSquares {
+    const safeSquares: SafeSquares = new Map<string, Coords[]>();
+
+    for (let x = 0; x < this.chessBoardSize; x++) {
+      for (let y = 0; y < this.chessBoardSize; y++) {
+        const piece: Piece | null = this.chessBoard[x][y];
+        if (!piece || piece.color !== this._playerColor) continue;
+
+        const pieceSafeSquares: Coords[] = [];
+
+        for (const { x: dx, y: dy } of piece.directions) {
+          let newX: number = x + dx;
+          let newY: number = y + dy;
+
+          if (!this.areCoordsValid(newX, newY)) continue;
+
+          let newPiece: Piece | null = this.chessBoard[newX][newY];
+          if (newPiece && newPiece.color === piece.color) continue;
+
+          //hay que restringir el movimiento de los peones en ciertas direcciones
+          if (piece instanceof Pawn) {
+            //el peon no se puede mover dos espacios hacia adelante si hay una pieza enfrente de el
+            if (dx === 2 || dx === -2) {
+              if (newPiece) continue;
+              if (this.chessBoard[newX + (dx === 2 ? -1 : 1)][newY]) continue;
+            }
+            //el peon no se puede mover un espacio hacia adelante si hay una pieza enfrente de el
+            if ((dx === 1 || dx === -1) && dy === 0 && newPiece) continue;
+            //el peon no se puede mover diagonalmente si no hay una pieza para comer o si hay una pieza de su mismo color
+            if (
+              (dx === 1 || dx === -1) &&
+              (!newPiece || piece.color === newPiece.color)
+            )
+              continue;
+          }
+
+          if (
+            piece instanceof Pawn ||
+            piece instanceof Knight ||
+            piece instanceof King
+          ) {
+            if (this.isPositionSafeAfterMove(piece, x, y, newX, newY)) {
+              pieceSafeSquares.push({ x: newX, y: newY });
+            }
+          } else {
+            while (this.areCoordsValid(newX, newY)) {
+              newPiece = this.chessBoard[newX][newY];
+
+              if (newPiece && newPiece.color === piece.color) break;
+
+              if (this.isPositionSafeAfterMove(piece, x, y, newX, newY)) {
+                pieceSafeSquares.push({ x: newX, y: newY });
+              }
+
+              if (newPiece !== null) break;
+
+              newX += dx;
+              newY += dy;
+            }
+          }
+        }
+        if (pieceSafeSquares.length) {
+          safeSquares.set(x + ',' + y, pieceSafeSquares);
+        }
+      }
+    }
+
+    return safeSquares;
+  }
+}
---
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { ChessBoard } from '../../chess-logic/chess-board';
+import {
+  Color,
+  Coords,
+  FENChar,
+  SafeSquares,
+  pieceImagePaths,
+} from '../../chess-logic/models';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { SelectedSquare } from './models';
+
+@Component({
+  selector: 'app-chess-board',
+  standalone: true,
+  imports: [NgFor, NgClass, NgIf],
+  templateUrl: './chess-board.component.html',
+  styleUrl: './chess-board.component.scss',
+})
+export class ChessBoardComponent {
+  public pieceImagePaths = pieceImagePaths;
+
+  private chessBoard = new ChessBoard();
+  public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
+  public get playerColor(): Color {
+    return this.chessBoard.playerColor;
+  }
+  public get safeSquares(): SafeSquares {
+    return this.chessBoard.safeSquares;
+  }
+  private selectedSquare: SelectedSquare = { piece: null };
+  private pieceSafeSquares: Coords[] = [];
+
+  public isSquareDark(x: number, y: number): boolean {
+    return ChessBoard.isSquareDark(x, y);
+  }
+
+  public isSquareSelected(x: number, y: number): boolean {
+    if (!this.selectedSquare.piece) return false;
+    return this.selectedSquare.x === x && this.selectedSquare.y === y;
+  }
+
+  public isSquareSafeForSelectedPiece(x: number, y: number): boolean {
+    return this.pieceSafeSquares.some(
+      (coords) => coords.x === x && coords.y === y
+    );
+  }
+
+  public selectingPiece(x: number, y: number): void {
+    const piece: FENChar | null = this.chessBoardView[x][y];
+    if (!piece) return;
+    this.selectedSquare = { piece, x, y };
+    this.pieceSafeSquares = this.safeSquares.get(x + ',' + y) || [];
+  }
+}
